2609113b4008de193e7462ee3afd7dda5f5babef
miya24n5 <sho.sakulai10@gmail.com>
2022-03-07 08:54:47 +0000

    [add]ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
+
   devise_for :users
-  get 'games/index'
-  get 'games/show'
-  get 'games/new'
-  get 'games/edit'
   root to: "homes#top"
+
+  resources :games, only: [:new, :index, :show, :edit, :create, :update, :dstroy] do
+    resources :game_comments, only: [:create, :dstroy]
+    resource :favorites, only: [:create, :dstroy]
+  end
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
