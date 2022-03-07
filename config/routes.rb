8a93f3594dc81ebf0835be2d619068a36a0d6c73
miya24n5 <sho.sakulai10@gmail.com>
2022-03-07 03:30:28 +0000

    [add]GamesController追加new,index,show,edit追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
+  get 'games/edit'
   root to: "homes#top"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
